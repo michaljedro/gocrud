4505a2b868ce8924a07d4ba9c7b1fd907ebfbd9e
Michał Jędro <michal.jedro@wp.pl>
2024-06-04 02:21:28 +0200

    changes

--- main.go
+++ main.go
@@ -6,41 +6,41 @@ import (
 
 	"github.com/gin-gonic/gin"
 )
-type todo struct {
-	ID string `json:"id"`
-	Item string `json:"title"`
+type user struct {
+	User string `json:"id"`
+	Email string `json:"title"`
 	Completed bool `json:"completed"`
 }
 
-var todos = []todo{
-	{ID: "1", Item: "Clean Room", Completed: false},
-	{ID: "2", Item: "Dirty Room", Completed: true},
-	{ID: "3", Item: "Clean windows", Completed: true},
+var Users = []user{
+	{User: "Michael Owen", Email: "Clean Room", Completed: false},
+	{User: "David Beckham", Email: "Dirty Room", Completed: true},
+	{User: "David Seaman", Email: "Clean windows", Completed: true},
 }
-func getTodos(context *gin.Context) {
-	context.IndentedJSON(http.StatusOK, todos)
+func getUsers(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, Users)
 }
-func addTodo(context *gin.Context) {
-	var newTodo todo
+func addUsers(context *gin.Context) {
+	var newTodo user
 	if err := context.BindJSON(&newTodo); err != nil {
 		return 
 	}
-	todos = append(todos, newTodo)
+	Users = append(Users, newTodo)
 	context.IndentedJSON(http.StatusCreated, newTodo)
 }
-func getTodo(context *gin.Context) {
+func getUser(context *gin.Context) {
 	id := context.Param("id")
-	todo, err := getTodoById(id)
+	todo, err := getUserByID(id)
 	if err != nil {
-		context.IndentedJSON(http.StatusNotFound, gin.H{"Message":"todo not found"})
+		context.IndentedJSON(http.StatusNotFound, gin.H{"Message":"user not found"})
 		return 
 	}
 	context.IndentedJSON(http.StatusOK, todo)
 }
 
-func toggleTodoStatus(context *gin.Context) {
+func toggleUserStatus(context *gin.Context) {
 	id := context.Param("id")
-	todo, err := getTodoById(id)
+	todo, err := getUserByID(id)
 	if err != nil {
 		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "not found"})
 		return
@@ -49,21 +49,21 @@ func toggleTodoStatus(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, todo)
 } 
 
-func getTodoById(id string) (*todo, error) {
-	for i,t := range todos {
-		if t.ID == id {
-			return &todos[i], nil
+func getUserByID(id string) (*user, error) {
+	for i,t := range Users {
+		if t.User == id {
+			return &Users[i], nil
 		}
 	}
-	return nil, errors.New("todos not found")
+	return nil, errors.New("users not found")
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/todos", getTodos)
-	router.GET("/todos/:id", getTodo)
-	router.PATCH("todos/:id", toggleTodoStatus)
-	router.POST("/todos", addTodo)
+	router.GET("/users", getUsers)
+	router.GET("/users/:id", getUser)
+	router.PATCH("users/:id", toggleUserStatus)
+	router.POST("/users", addUsers)
 	router.Run("localhost:9090")
 }
 
